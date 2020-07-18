cd3c0f18d73bac400ca78cec09a92f63c793a49a
Jitendra singh Shahi <jshahi0105@conestogac.on.ca>
2020-07-18 14:56:38 -0400

    routes added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const authCtrl = require('../controllers/auth');
+const userCtrl = require('../controllers/user');
+const middleware = require('../controllers/verifyToken');
+
+router.post('/user/register', userCtrl.register);
+router.post('/user/login', authCtrl.login);
+router.post('/validateToken', authCtrl.isValidToken);
+
+router.get('/user-profile', middleware, userCtrl.getUserById);
 
 module.exports = router;
