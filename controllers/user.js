e50afc159ba4297d8abf54428db88bdb2e746791
Jitendra singh Shahi <jshahi0105@conestogac.on.ca>
2020-07-18 19:52:05 -0400

    Use Controller for registration and activation of account

--- controllers/user.js
+++ controllers/user.js
@@ -2,14 +2,24 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const db = require('../models');
 const config = require('../config');
+const email = require('../helpers/sendgrid');
 
-module.exports = {
-    register: async (req, res) => {
-        if (!req.body.email || !req.body.password || !req.body.first_name) {
-            return res.status(400).json({ error: true, message: 'email, password or first_name parameters are missing.' });
+class User {
+    constructor() {
+        console.log('hello user controller');
+        this.register = this.register.bind(this);
+        this.activate = this.activate.bind(this);
+    }
+
+    static async register(req, res) {
+        if (!req.body.email || !req.body.password || !req.body.confirm_password
+            || !req.body.first_name) {
+            return res.status(400).json({ error: true, message: 'email, password, confirm_password or first_name parameters are missing.' });
         }
         if (req.body.password.length < 8) {
-            return res.status(400).json({ error: true, message: 'Password length must be 8.' });
+            return res.status(400).json({ error: true, message: 'password length must be 8.' });
+        } else if (req.body.password !== req.body.confirm_password) {
+            return res.status(400).json({ error: true, message: 'password and confirm_password doesn\'t match' });
         }
         const userData = await this.getUserByEmail(req, res);
         if (userData) {
@@ -19,66 +29,93 @@ module.exports = {
         const hashedPassword = bcrypt.hashSync(req.body.password, 8);
         const lastName = req.body.last_name ? req.body.last_name : '';
 
-        db.Users.create({
+        await db.Users.create({
             first_name: req.body.first_name,
             last_name: lastName,
             email: req.body.email,
             password: hashedPassword,
-        },
-        (err, user) => {
+        }, (err, user) => {
             if (err) {
                 return res.status(500).json({
                     error: true,
                     message: 'There was a problem registering the user.',
                 });
             }
+
             const token = jwt.sign({ id: user._id }, config.secret, {
                 expiresIn: '720h', // expires in 30 days
-            });
-            const userInfo = {
-                first_name: user.first_name,
-                last_name: user.last_name,
-                email: user.email,
-                id: user._id,
+            }).replace(/\./g, '_');
+            const activationUrl = `http://${req.get('host')}/activate/${token}`;
+            const htmlMessage = `Hi ${user.first_name}, <br/><br/>
+            Welcome to ECommerce Please Click <a href="${activationUrl}">Here</a> to activate account.
+            If you are having problem please use below url in browser.<br/><br/>
+            ${activationUrl}<br/><br/>Thanks,<br/>
+            Team Ecommerce`;
+
+            email.sendMail(user.email, 'Account Activation Email', '', htmlMessage);
+
+            const info = {
+                message: 'Please check your email to activate account',
             };
-            return res.status(200).json({ auth: true, token, userInfo });
+            return res.status(200).json({ success: true, info });
         });
-        return res.status(500).json({ error: true, message: 'something went wrong.' });
-    },
+    }
 
-    getUserByEmail: async (req, res) => {
-        if (!req.body.email) {
-            return res.status(400).json({ error: true, message: 'email parameters is missing.' });
-        }
-        await db.Users.findOne({ email: req.body.email }, (err, user) => {
+    static async activate(req, res) {
+        const activationToken = req.originalUrl.split('/').slice(-1)[0].replace(/_/g, '.');
+        jwt.verify(activationToken, config.secret, (err, decoded) => {
             if (err) {
                 return res.status(500)
                     .json({
                         error: true,
-                        message: 'Something went wrong.',
+                        message: 'Failed to activate account.',
                     });
             }
-            if (!user) return null;
-            return user;
+            db.Users.findOneAndUpdate({ _id: decoded.id }, { is_active: true }, { new: true })
+                // eslint-disable-next-line no-unused-vars
+                .exec((error, user) => {
+                    if (error) {
+                        res.status(500).json({ success: true, message: 'Something wrong happen.' });
+                    }
+                    res.status(200).json({ success: true, message: 'Account activated' });
+                });
         });
-        return null;
-    },
+    }
 
-    getUserById: async (req, res) => {
-        if (!req.body.email) {
-            return res.status(400).json({ error: true, message: 'email parameters is missing.' });
-        }
-        await db.Users.findOne({ _id: req.body.user_id }, (err, user) => {
-            if (err) {
-                return res.status(500)
-                    .json({
+    static async getUserById(req, res) {
+        return new Promise((resolve, reject) => {
+            if (!req.body.user_id) {
+                return res.status(400).json({ error: true, message: 'email parameters is missing.' });
+            }
+            db.Users.findOne({ _id: req.body.user_id }).exec((err, user) => {
+                if (err) {
+                    reject(new Error({
                         error: true,
                         message: 'Something went wrong.',
-                    });
-            }
-            if (!user) return null;
-            return user;
+                    }));
+                }
+                if (!user) resolve(null);
+                resolve(user);
+            });
         });
-        return null;
-    },
-};
+    }
+
+    static async getUserByEmail(req, res) {
+        return new Promise((resolve, reject) => {
+            if (!req.body.email) {
+                return res.status(400).json({ error: true, message: 'email parameters is missing.' });
+            }
+            db.Users.findOne({ email: req.body.email }).exec((err, user) => {
+                if (err) {
+                    reject(new Error({
+                        error: true,
+                        message: 'Something went wrong.',
+                    }));
+                }
+                if (!user) resolve(null);
+                resolve(user);
+            });
+        });
+    }
+}
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const authCtrl = require('../controllers/auth');
 const userCtrl = require('../controllers/user');
 const middleware = require('../controllers/verifyToken');
 
-router.post('/user/register', userCtrl.register);
+router.get('/user/activate/*', userCtrl.activate.bind(userCtrl));
+
+router.post('/user/register', userCtrl.register.bind(userCtrl));
 router.post('/user/login', authCtrl.login);
 router.post('/validateToken', authCtrl.isValidToken);
 
-router.get('/user-profile', middleware, userCtrl.getUserById);
+router.get('/user/user-profile', middleware, userCtrl.getUserById);
 
 module.exports = router;
